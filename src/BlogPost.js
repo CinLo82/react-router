aa2567fdb08f46c98cb0c834b28b88991fee8265
CinLo <cinlos3382@gmail.com>
2022-11-11 10:08:39 -0300

    se agregan paginas y rutas de navegacion

---
+++ src/BlogPost.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { blogData } from './blogData';
+
+function BlogPost () {
+    const navigate = useNavigate();
+    const { slug } = useParams();
+
+    const returnToBlog = () => {
+        navigate('/blog');
+    };
+
+    const blogpost = blogData.find(post => post.slug === slug);
+
+    return (
+        <>
+            <h2>{blogpost.title}</h2>
+            <button onClick={returnToBlog}>Volver al Blog</button>
+            <p>{blogpost.author}</p>
+            <p>{blogpost.content}</p>
+            
+        </>
+    );
+}
+
+export { BlogPost };
---
+++ src/blogData.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const blogData = [];
+blogData.push({
+    title: '¿Que es React?',
+    slug: 'que-es-react',
+    content: 'React es el mejor framework de Javascript.',
+    author: 'juandc',
+});
+blogData.push({
+    title: '¿Que es Vue?',
+    slug: 'que-es-vue',
+    content: 'Vue es el mejor framework de Javascript.',
+    author: 'diannerd',
+});
+blogData.push({
+    title: '¿Que es Angular?',
+    slug: 'que-es-angular',
+    content: 'Angular es el mejor framework de Javascript.',
+    author: 'nicobytes',
+})
+
+export { blogData };
